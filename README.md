7713711ee94daa413041bf2fe089bc92815b42ca
Manoj <manumanoj0010@gmail.com>
2021-05-30 13:51:14 +0530

    updated for non pattern files

--- README.md
+++ README.md
@@ -11,9 +11,13 @@ A python script which helps in sorting your bulk images according to the date of
 
 ### Description
 
-- Every image has some data which includes date of the photo taken, location, device through which photo was taken etc. This data is called Exif data (Exchangeable image file format). Our task is to extract the date from the exif data and sort according to it
+- Every image has some data which includes date of the photo taken, location, device through which photo was taken etc. This data is called Exif data **(Exchangeable image file format)**. Our task is to extract the date from the exif data and sort according to it
 
-- Images which has been shared through whatsapp will not have any exif data as whatsapp remove those data inorder to reduce the file size. Inorder to tackle that I have extracted the date from the file name. whatsapp follows the certain naming pattern Example** IMG-20191122-WA0023.jpg** here 2019 represent year 11 represents month and 22 represents date and WA represents whatsapp.
+- Images which has been shared through whatsapp will not have any exif data as whatsapp remove those data inorder to reduce the file size. Inorder to tackle that I have extracted the date from the file name. whatsapp follows the certain naming pattern Example **IMG-20191122-WA0023.jpg** here 2019 represent year 11 represents month and 22 represents date and WA represents whatsapp.
+
+- If a image doesnot follow any pattern name like **IMG-20191122-WA0023.jpg** or **snapchat-2019122-0023.jpg** then the date is extracted from the inbuilt os module i.e creation date of the image.
+
+- I can simply follow the 3rd step instead of 1st and 2nd step but **Creation dates do not necessarily reflect when a file was actually created or image was taken. Rather, creation date stamps indicate when a file came to exist on a particular storage medium, such as a hard drive. Creation dates can thus indicate when a user or computer process created a file. However, they can also reflect the date and time that a file was copied onto a particular storage medium**. I need the actual date of image when the image was taken thus the priority order goes like this.
 
 ### How to Run
 #### Requirements
--- image_sorter.py
+++ image_sorter.py
@@ -4,23 +4,36 @@ import glob
 import datetime
 import shutil
 
-
-class SortFiles():
+class SortImages():
+    """ Organize images into folders according to the date time """
+    
     def __init__(self):
+        """ constructor methond """
+        
         self.months = {1:'JAN', 2:'FEB', 3:'MAR', 4:'APR', 5:'MAY', 6:'JUNE', 7:'JULY', 8:'AUG', 9:'SEP', 10:'OCT', 11:'NOV', 12:'DEC'}
         self.current_dir = os.getcwd()
+        self.image_extensions = ['jpeg','jpg','png']
     
-    def main(self):
+    def sort_by_date(self):
+        """ function will take extract the dates from the images """
+        
         files = glob.glob("*")
         for file in files:
             isFIle = os.path.isfile(self.current_dir + '/' + file)
+            # checking whether its a file or directory
             if isFIle:
                 extension = file.split(sep=".")
-                if extension[1].lower() == 'jpeg' or extension[1].lower() == 'jpg' or extension[1].lower() =='png':
+                # if a file doesnot have any extension like LICENSE file
+                if len(extension)<2:
+                    continue
+                file_extension = extension[1].lower()
+                if file_extension in self.image_extensions:
                     image = Image.open(file)
                     exifdata = image.getexif()
                     image.close()
+                    # 306 represent the date time code in exif data
                     dateTime = exifdata.get(306)
+                    # if image has exif data i.e date and time
                     if dateTime:
                         dateTimeArray = dateTime.split()
                         date, time = dateTimeArray[0], dateTimeArray[1]
@@ -28,38 +41,55 @@ class SortFiles():
                         timeArray = time.split(':')
                         year, month, date = dateArray[0], dateArray[1], dateArray[2]
                         hour, min, sec = timeArray[0], timeArray[1], timeArray[2]
-                        new_name = year + '-' + month + '-' + date + '@'+ '1' + '.' + extension[1].lower()
+                        # format for image name (new name)
+                        new_name = year + '-' + month + '-' + date + '@'+ '1' + '.' + file_extension
+                        # new directory path
                         new_dir = self.current_dir + "/Photos/" + year + "/" + self.months.get(int(month)) + "/" + date
-                        self.sorter(file, new_dir, new_name, year, month, date, extension)
-                        
+                        self.file_organiser(file, new_dir, new_name, year, month, date, extension)
+                    
+                    # if image doesnot have any exif data
                     else:
+                        # files whose naming pattern is IMG-20191122-WA0023.jpg
                         fileName = file.split(sep="-")
+                        #files whose naming pattern is IMG_20191122_0023.jpg
                         if len(fileName)<2:
                             fileName = file.split(sep="_")
-                        dateArray = fileName[1]
-                        year, month, date = dateArray[:4], dateArray[4:6], dateArray[6:8]
-                        new_name = year + '-' + month + '-' + date + '@' + '1' + '.' + extension[1].lower()
+                        # files which doesnot have any naming pattern
+                        if len(fileName)<2:                           
+                            ti_m = os.path.getctime(file)
+                            date_stamp = datetime.date.fromtimestamp(ti_m)
+                            dateArray = str(date_stamp).split(sep="-")
+                            year, month, date =dateArray[0], dateArray[1], dateArray[2]
+                        else:
+                            dateArray = fileName[1]
+                            year, month, date = dateArray[:4], dateArray[4:6], dateArray[6:8]
+                            
+                        new_name = year + '-' + month + '-' + date + '@' + '1' + '.' + file_extension
                         new_dir = self.current_dir + "/Photos/" + year + "/" + self.months.get(int(month)) + "/" + date
-                        self.sorter(file, new_dir, new_name, year, month, date, extension)
+                        self.file_organiser(file, new_dir, new_name, year, month, date, extension)
                         
-    def sorter(self, file, new_dir, new_name, year, month, date, extension):
+    def file_organiser(self, file, new_dir, new_name, year, month, date, extension):
+        """ organize the image files into folders by date """
+        # creating a new directory if it doesnot exists
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
             
         checkFile = os.path.isfile(new_dir + '/' + new_name)
         
+        # creating a new file name if a similar file already exists in folder. the file will chanage to 2020-02-01@1 2020-02-01@2 and so on
         i=2
         while (checkFile):
             new_name = year + '-' + month + '-' + date + '@' + str(i) + '.' + extension[1].lower()
             checkFile = os.path.isfile(new_dir + '/' + new_name)
             i+=1
-            
+        
+        #renaming the file and moving the file to a specified folder
         os.rename(file, new_name)
         shutil.move(new_name, new_dir)
         return 
                         
-a = SortFiles()
-a.main()
-                        
-                        
-                        
+
+if __name__ == "__main__":
+    a = SortImages()
+    a.sort_by_date()
+    
